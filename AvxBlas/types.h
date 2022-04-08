52f1760ce10cc795bbeef727bd8de653adc42143
tk-yoshimura <yoshimura8192@gmail.com>
2022-04-08 14:35:13 +0900

    + type xntypes

--- AvxBlas/types.h
+++ AvxBlas/types.h
@@ -82,3 +82,143 @@ struct __m256dx5 {
     constexpr __m256dx5(__m256d imm0, __m256d imm1, __m256d imm2, __m256d imm3, __m256d imm4)
         : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4) { }
 };
+
+struct __m256x6 {
+    __m256 imm0, imm1, imm2, imm3, imm4, imm5;
+
+    constexpr __m256x6(__m256 imm0, __m256 imm1, __m256 imm2, __m256 imm3, __m256 imm4, __m256 imm5)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4), imm5(imm5) { }
+};
+
+struct __m256dx6 {
+    __m256d imm0, imm1, imm2, imm3, imm4, imm5;
+
+    constexpr __m256dx6(__m256d imm0, __m256d imm1, __m256d imm2, __m256d imm3, __m256d imm4, __m256d imm5)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4), imm5(imm5) { }
+};
+
+struct __m256x7 {
+    __m256 imm0, imm1, imm2, imm3, imm4, imm5, imm6;
+
+    constexpr __m256x7(__m256 imm0, __m256 imm1, __m256 imm2, __m256 imm3, __m256 imm4, __m256 imm5, __m256 imm6)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4), imm5(imm5), imm6(imm6) { }
+};
+
+struct __m256dx7 {
+    __m256d imm0, imm1, imm2, imm3, imm4, imm5, imm6;
+
+    constexpr __m256dx7(__m256d imm0, __m256d imm1, __m256d imm2, __m256d imm3, __m256d imm4, __m256d imm5, __m256d imm6)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4), imm5(imm5), imm6(imm6) { }
+};
+
+struct __m256x8 {
+    __m256 imm0, imm1, imm2, imm3, imm4, imm5, imm6, imm7;
+
+    constexpr __m256x8(__m256 imm0, __m256 imm1, __m256 imm2, __m256 imm3, __m256 imm4, __m256 imm5, __m256 imm6, __m256 imm7)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4), imm5(imm5), imm6(imm6), imm7(imm7) { }
+};
+
+struct __m256dx8 {
+    __m256d imm0, imm1, imm2, imm3, imm4, imm5, imm6, imm7;
+
+    constexpr __m256dx8(__m256d imm0, __m256d imm1, __m256d imm2, __m256d imm3, __m256d imm4, __m256d imm5, __m256d imm6, __m256d imm7)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4), imm5(imm5), imm6(imm6), imm7(imm7) { }
+};
+
+struct __m128x2 {
+    __m128 imm0, imm1;
+
+    constexpr __m128x2(__m128 imm0, __m128 imm1)
+        : imm0(imm0), imm1(imm1) { }
+};
+
+struct __m128dx2 {
+    __m128d imm0, imm1;
+
+    constexpr __m128dx2(__m128d imm0, __m128d imm1)
+        : imm0(imm0), imm1(imm1) { }
+};
+
+struct __m128x3 {
+    __m128 imm0, imm1, imm2;
+
+    constexpr __m128x3(__m128 imm0, __m128 imm1, __m128 imm2)
+        : imm0(imm0), imm1(imm1), imm2(imm2) { }
+};
+
+struct __m128dx3 {
+    __m128d imm0, imm1, imm2;
+
+    constexpr __m128dx3(__m128d imm0, __m128d imm1, __m128d imm2)
+        : imm0(imm0), imm1(imm1), imm2(imm2) { }
+};
+
+struct __m128x4 {
+    __m128 imm0, imm1, imm2, imm3;
+
+    constexpr __m128x4(__m128 imm0, __m128 imm1, __m128 imm2, __m128 imm3)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3) { }
+};
+
+struct __m128dx4 {
+    __m128d imm0, imm1, imm2, imm3;
+
+    constexpr __m128dx4(__m128d imm0, __m128d imm1, __m128d imm2, __m128d imm3)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3) { }
+};
+
+struct __m128x5 {
+    __m128 imm0, imm1, imm2, imm3, imm4;
+
+    constexpr __m128x5(__m128 imm0, __m128 imm1, __m128 imm2, __m128 imm3, __m128 imm4)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4) { }
+};
+
+struct __m128dx5 {
+    __m128d imm0, imm1, imm2, imm3, imm4;
+
+    constexpr __m128dx5(__m128d imm0, __m128d imm1, __m128d imm2, __m128d imm3, __m128d imm4)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4) { }
+};
+
+struct __m128x6 {
+    __m128 imm0, imm1, imm2, imm3, imm4, imm5;
+
+    constexpr __m128x6(__m128 imm0, __m128 imm1, __m128 imm2, __m128 imm3, __m128 imm4, __m128 imm5)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4), imm5(imm5) { }
+};
+
+struct __m128dx6 {
+    __m128d imm0, imm1, imm2, imm3, imm4, imm5;
+
+    constexpr __m128dx6(__m128d imm0, __m128d imm1, __m128d imm2, __m128d imm3, __m128d imm4, __m128d imm5)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4), imm5(imm5) { }
+};
+
+struct __m128x7 {
+    __m128 imm0, imm1, imm2, imm3, imm4, imm5, imm6;
+
+    constexpr __m128x7(__m128 imm0, __m128 imm1, __m128 imm2, __m128 imm3, __m128 imm4, __m128 imm5, __m128 imm6)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4), imm5(imm5), imm6(imm6) { }
+};
+
+struct __m128dx7 {
+    __m128d imm0, imm1, imm2, imm3, imm4, imm5, imm6;
+
+    constexpr __m128dx7(__m128d imm0, __m128d imm1, __m128d imm2, __m128d imm3, __m128d imm4, __m128d imm5, __m128d imm6)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4), imm5(imm5), imm6(imm6) { }
+};
+
+struct __m128x8 {
+    __m128 imm0, imm1, imm2, imm3, imm4, imm5, imm6, imm7;
+
+    constexpr __m128x8(__m128 imm0, __m128 imm1, __m128 imm2, __m128 imm3, __m128 imm4, __m128 imm5, __m128 imm6, __m128 imm7)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4), imm5(imm5), imm6(imm6), imm7(imm7) { }
+};
+
+struct __m128dx8 {
+    __m128d imm0, imm1, imm2, imm3, imm4, imm5, imm6, imm7;
+
+    constexpr __m128dx8(__m128d imm0, __m128d imm1, __m128d imm2, __m128d imm3, __m128d imm4, __m128d imm5, __m128d imm6, __m128d imm7)
+        : imm0(imm0), imm1(imm1), imm2(imm2), imm3(imm3), imm4(imm4), imm5(imm5), imm6(imm6), imm7(imm7) { }
+};
