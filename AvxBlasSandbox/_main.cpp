37ae77f05e35aa3e97109785276afba3835585ec
tk-yoshimura <yoshimura8192@gmail.com>
2022-04-20 07:06:29 +0900

    sandbox sort

--- AvxBlasSandbox/_main.cpp
+++ AvxBlasSandbox/_main.cpp
@@ -34,6 +34,38 @@ __forceinline __m128 _mm_sort_ps(__m128 x) {
     return x;
 }
 
+__forceinline __m256 _mm256_evensort_ps(__m256 x) {
+    __m256 y = _mm256_permute_ps(x, _MM_PERM_CDAB);
+    __m256 c = _mm256_permute_ps(_mm256_cmp_ps(x, y, _CMP_GT_OQ), _MM_PERM_CCAA);
+    __m256 z = _mm256_blendv_ps(x, y, c);
+
+    return z;
+}
+
+__forceinline __m256 _mm256_oddsort_ps(__m256 x) {
+    const __m256 xormask = _mm256_castsi256_ps(_mm256_setr_epi32(~0u, 0, 0, 0, 0, 0, 0, ~0u));
+    const __m256i perm = _mm256_setr_epi32(7, 2, 1, 4, 3, 6, 5, 0);
+    const __m256i permcmp = _mm256_setr_epi32(7, 1, 1, 3, 3, 5, 5, 7);
+
+    __m256 y = _mm256_permutevar8x32_ps(x, perm);
+    __m256 c = _mm256_xor_ps(xormask, _mm256_permutevar8x32_ps(_mm256_cmp_ps(x, y, _CMP_GT_OQ), permcmp));
+    __m256 z = _mm256_blendv_ps(x, y, c);
+
+    return z;
+}
+
+__forceinline __m256 _mm256_sort_ps(__m256 x) {
+    x = _mm256_oddsort_ps(x);
+    x = _mm256_evensort_ps(x);
+    x = _mm256_oddsort_ps(x);
+    x = _mm256_evensort_ps(x);
+    x = _mm256_oddsort_ps(x);
+    x = _mm256_evensort_ps(x);
+    x = _mm256_oddsort_ps(x);
+
+    return x;
+}
+
 int main(){
     __m128 x1234 = _mm_setr_ps(1, 2, 3, 4);
     __m128 x1243 = _mm_setr_ps(1, 2, 4, 3);
@@ -90,6 +122,13 @@ int main(){
     __m128 y4312 = _mm_sort_ps(x4312);
     __m128 y4231 = _mm_sort_ps(x4231);
     __m128 y4321 = _mm_sort_ps(x4321);
+
+    __m256 x87654321 = _mm256_setr_ps(8, 7, 6, 5, 4, 3, 2, 1);
+    __m256 x81234567 = _mm256_setr_ps(8, 1, 2, 3, 4, 5, 6, 7);
+
+    __m256 y87654321 = _mm256_sort_ps(x87654321);
+    __m256 y81234567 = _mm256_sort_ps(x81234567);
+
 
     printf("end");
     getchar();
